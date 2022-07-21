6099e5c3499d95f0d99419bfa84c5ad4b8c64e3b
allenkays <allenkays@tutanota.com>
2022-07-21 15:27:39 -0400

    slack bot file upload code

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3836070680288-3813091294274-UF94gzW4u0Rz7typQO9Y1FA1")
+	os.Setenv("CHANNEL_ID", "C03PW8VGE4S")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"DRS Interview Questions.pdf"}
+
+	for i := range fileArr {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+}
